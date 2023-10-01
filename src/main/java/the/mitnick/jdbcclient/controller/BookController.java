fd1813346bf10b7ea75eaad4da3d142c7ff4f3d8
Rodrigue YAO <yaotyrone@gmail.com>
2023-10-01 22:00:11 +0200

    First commit

--- src/main/java/the/mitnick/jdbcclient/controller/BookController.java
+++ src/main/java/the/mitnick/jdbcclient/controller/BookController.java
@@ -2,13 +2,11 @@ package the.mitnick.jdbcclient.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import the.mitnick.jdbcclient.entity.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -21,8 +19,41 @@ public class BookController {
     public String addNewBook(@RequestBody Book book) {
 
         jdbcClient.sql("INSERT INTO book(id, name, title) VALUES(?,?,?)")
-                .param(List.of(book.getId(), book.getName(), book.getTitle()))
+                .params(List.of(book.getId(), book.getName(), book.getTitle()))
                 .update();
         return "A new book added";
     }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return jdbcClient.sql("SELECT * FROM book")
+                .query(Book.class)
+                .list();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Book> getBookById(@PathVariable int id) {
+        return jdbcClient.sql("SELECT * FROM book WHERE id =: id")
+                .param("id", id)
+                .query(Book.class).optional();
+    }
+
+    @PutMapping("/{id}")
+    public String updateBook(@PathVariable int id, @RequestBody Book book) {
+        jdbcClient.sql("UPDATE book set name = ?, title = ? WHERE id = ?")
+                .params(List.of(book.getName(),book.getTitle(), id))
+                .update();
+
+        return "Book has been modified";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable int id) {
+        jdbcClient.sql("DELETE FROM book WHERE id=: id")
+                .param("id", id)
+                .update();
+
+        return "Book has been deleted in the system";
+    }
+
 }
